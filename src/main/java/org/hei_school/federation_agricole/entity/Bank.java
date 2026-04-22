52bfcbc09c5897481c36fb5b753bd0c3472178c9
Eric-Ravonjiniaina <rnvjeric@gmail.com>
2026-04-22 16:39:00 +0300

    implement functional bankaccount with enum

---
+++ src/main/java/org/hei_school/federation_agricole/entity/Bank.java
@@ -0,0 +1,13 @@
+package org.hei_school.federation_agricole.entity;
+
+public enum Bank {
+    BRED,
+    MCB,
+    BMOI,
+    BOA,
+    BGFI,
+    AFG,
+    ACCES_BAQUE,
+    BAOBAB,
+    SIPEM;
+}
---
+++ src/main/java/org/hei_school/federation_agricole/entity/BankAccount.java
@@ -0,0 +1,10 @@
+package org.hei_school.federation_agricole.entity;
+
+public class BankAccount extends FinancialAccount{
+    private String Name;
+    private Bank bankName;
+    private String bankCode;
+    private String branchCode;
+    private String accountNumber;
+    private String keyRIB;
+}
